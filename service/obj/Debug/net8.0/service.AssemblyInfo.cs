fde5e99fb266e8830b8afa8e9bc5d641e21dd126
DaniRonda <90683193+DaniRonda@users.noreply.github.com>
2023-10-11 17:18:11 +0200

    .

--- service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a426f35c0545ea14cbb2c93d7cbfd2d3ff702993")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
