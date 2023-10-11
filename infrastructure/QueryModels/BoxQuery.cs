2c9aecf4982ad9abbe26e6ca7482f1016591e943
DaniRonda <90683193+DaniRonda@users.noreply.github.com>
2023-10-12 01:29:30 +0200

    .

---
+++ infrastructure/QueryModels/BoxQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace infrastructure.QueryModels;
+
+public class BoxQuery
+{
+    public int BoxId { get; set; }
+    public string BoxName { get; set; }
+    public string BoxDescription  { get; set; }
+    public string BoxImgUrl { get; set; } 
+}
